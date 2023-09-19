e806ab109a8a85ee447e0804c6a8da9dd300246e
Kevin Miguet <kevin@jollycommerce.io>
2023-09-19 12:19:37 +0700

    index

---
+++ pixel.js
@@ -0,0 +1,41 @@
+const shopName = "EXAMPLE";
+
+async function standardEcomTracking(data) {
+  const url =
+    "https://standard-ecom.netlify.app/.netlify/functions/sync_klaviyo_ltv";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("API request failed");
+    }
+    const data = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+analytics.subscribe("checkout_completed", (event) => {
+  const email = event.data.checkout.email;
+  if (!email) {
+    return; // no email, no tracking :(
+  }
+  const orderTotalPrice = event.data.checkout.totalPrice.amount;
+
+  const data = {
+    event_type: "checkout_completed",
+    event_data: {
+      email,
+      orderTotalPrice,
+      shopName,
+    },
+  };
+
+  standardEcomTracking(data);
+});
